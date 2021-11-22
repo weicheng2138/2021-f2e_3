177ed7306ab8868334203baeebc64668d34cf58a
weicheng <weicheng2138@gmail.com>
2021-11-22 22:56:44 +0800

    fix router problem in main.js

--- src/main.js
+++ src/main.js
@@ -5,9 +5,10 @@ import { routes } from "./routes.js";
 import { createRouter, createWebHistory } from "vue-router";
 
 const app = createApp(App);
+console.log(import.meta.env);
 
 const router = createRouter({
-    history: createWebHistory("/2021-f2e_2/"),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes,
 });
 
