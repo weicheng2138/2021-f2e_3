7237ee0df1b9e2cf23611f8abd0ebb2f361f06d8
keckhung <p10361880@pic.net.tw>
2021-11-22 16:51:43 +0800

    logo link test

--- src/main.js
+++ src/main.js
@@ -3,12 +3,11 @@ import "./tailwind.css";
 import App from "./App.vue";
 import { routes } from "./routes.js";
 import { createRouter, createWebHistory } from "vue-router";
-const BASE_URL = import.meta.env.BASE_URL;
 
 const app = createApp(App);
 
 const router = createRouter({
-	history: createWebHistory(BASE_URL),
+	history: createWebHistory(),
 	routes,
 });
 
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import Home from "./views/Home.vue";
 import About from "./views/About.vue";
 import NotFound from "./views/NotFound.vue";
+const BASE_URL = import.meta.env.BASE_URL;
 
 /** @type {import('vue-router').RouterOptions['routes']} */
 export const routes = [
-	{ path: "/", component: Home, meta: { title: "Home" } },
+	{ path: BASE_URL + "/", component: Home, meta: { title: "Home" } },
 	{
-		path: "/about",
+		path: BASE_URL + "/about",
 		meta: { title: "About" },
 		component: About,
 		// example of route level code-splitting
@@ -14,5 +15,5 @@ export const routes = [
 		// which is lazy-loaded when the route is visited.
 		// component: () => import('./views/About.vue')
 	},
-	{ path: "/:path(.*)", component: NotFound },
+	{ path: BASE_URL + "/:path(.*)", component: NotFound },
 ];
